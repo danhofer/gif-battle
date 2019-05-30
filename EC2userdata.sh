500684ba068e0f40c3c3b35fa84caf3026da8f27
Dan Hofer <danhofer@gmail.com>
2019-05-29 23:50:46 -0400

    update EC2 userdata script

--- EC2userdata.sh
+++ EC2userdata.sh
@@ -16,22 +16,16 @@ nvm install node
 
 yum install git -y
 
-git clone https://github.com/danhofer/react-gif-game.git /home/ec2-user/react-gif-game
+git clone https://github.com/danhofer/gif-battle.git /home/ec2-user/gif-battle
 
-cd /home/ec2-user/react-gif-game
+echo "REACT_APP_PUBLIC_IP=$(curl ifconfig.me)" >> /home/ec2-user/gif-battle/frontend/.env.production
 
-echo "REACT_APP_PUBLIC_IP=$(curl ifconfig.me)" >> /home/ec2-user/react-gif-game/.env.production
+docker build -t frontend /home/ec2-user/gif-battle/frontend
 
-docker build -t react-gif-game /home/ec2-user/react-gif-game
+docker run -p 80:5000 -d frontend
 
-docker run -p 80:5000 -d react-gif-game
+docker build -t backend /home/ec2-user/gif-battle/backend
 
-git clone https://github.com/danhofer/gif-game-server.git /home/ec2-user/gif-game-server
-
-cd /home/ec2-user/gif-game-server
-
-docker build -t gif-game-server .
-
-docker run -p 8080:8080 -d gif-game-server
+docker run -p 8080:8080 -d backend
 
 aws ec2 create-tags --region $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/[a-z]$//') --resources $(curl http://169.254.169.254/latest/meta-data/instance-id) --tags Key='Name',Value='userdataComplete'
