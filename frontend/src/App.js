e34bb80d614ea9dbe57a41218d015d72acec9926
Dan Hofer <danhofer@gmail.com>
2019-06-07 00:11:35 -0400

    reset frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            view: 'game',
+            view: 'staging',
             roomId: '',
             isGameLeader: false,
             players: [],
